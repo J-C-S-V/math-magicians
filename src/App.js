bbc4a1d91ae9c4d1fc06b2c73e83a5029a8531c2
Juan Sanchez <juansan1993@hotmail.com>
2023-05-02 12:59:32 -0500

    Add routes library and code to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quote from './components/Quote';
 import './App.css';
 
+function Layout() {
+  return (
+    <>
+      <h1>Math Magicians</h1>
+      <Outlet />
+    </>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <div className="main">
-        <h1 className="title">Math magicians</h1>
-        <Calculator />
-      </div>
-      <Quote />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
