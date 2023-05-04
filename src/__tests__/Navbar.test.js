8d5d997755170cce4a904d63af217b25bffac4eb
Juan Sanchez <juansan1993@hotmail.com>
2023-05-04 12:11:46 -0500

    Fix linter errors

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,4 +1,3 @@
-import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Navbar from '../components/Navbar';
